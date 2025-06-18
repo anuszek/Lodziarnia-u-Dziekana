20f502d06efbc61bc3213af8321f54720f39ec13
anuszek <153279378+anuszek@users.noreply.github.com>
2025-06-18 11:52:27 +0200

    omh maps

---
+++ Lodziarnia-u-Dziekana/app/(tabs)/stalls.tsx
@@ -0,0 +1,7 @@
+import {OmhMapsModule} from '@openmobilehub/maps-core';
+
+OmhMapsModule.initialize({
+  iosProvider: <PROVIDER_FOR_IOS_DEVICES>,
+  gmsProvider: <PROVIDER_FOR_ANDROID_GMS_DEVICES>,
+  nonGmsProvider: <PROVIDER_FOR_ANDROID_NON_GMS_DEVICES>,
+});
---
+++ Lodziarnia-u-Dziekana/react-native.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  dependencies: {
+    "@openmobilehub/maps-core": {
+      platforms: {
+        ios: null,
+      },
+    },
+    "@react-native-community/geolocation": {
+      platforms: {
+        android: null,
+      },
+    },
+    "react-native-maps": {
+      platforms: {
+        android: null,
+      },
+    },
+  },
+};
